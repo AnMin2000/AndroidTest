7ea4df2243578d65a4cbc122cf8ed3fc7d3c076e
AnMin <iededg@naver.com>
2023-08-05 22:40:53 +0900

    test2 버튼 클릭시 텍스트를 갖고 화면 이동.

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -11,39 +11,28 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    //    EditText et_id;
-//    Button btn_test;
-//    Button btn_test2;
     private Button btn_move;
+    private EditText et_test;
+    private String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { // 어플을 처음 실행 했을 때 생명주기
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        et_test = findViewById(R.id.et_test);
+
+
         btn_move = findViewById(R.id.btn_move);
         btn_move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                str = et_test.getText().toString();
                 Intent intent = new Intent(MainActivity.this, SubActivity.class); // 해당 컴포넌트가 무엇을 할것인지 설정
+                intent.putExtra("str", str); // 별명, 실제 변수명
                 startActivity(intent); // 실제 액티비티 이동 해주는 구문
             }
         });
     }
 }
-//        et_id = findViewById(R.id.et_id); //특정 아이디를 이쪽으로 부여해라 약간 스윙의 get 같은거
-//        btn_test = findViewById(R.id.btn_test);
-//        btn_test2 = findViewById(R.id.button);
-//
-//        btn_test.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                et_id.setText("홍드로이드");
-//            }
-//        });
-//        btn_test2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                et_id.setText("짜장면");
-//            }
-//        });
+
